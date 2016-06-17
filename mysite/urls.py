87e141bccb4c911ed4dbb895ca306b5c9b741dbb
jiangxianfu <steven_jiangxf@hotmail.com>
2016-06-18 02:33:53 +0800

    fix

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,12 +17,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf import settings
-from django.contrib.staticfiles import views
+from django.conf.urls.static import static
 from mysite import views
 
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^admin/', admin.site.urls),
-    url(r'^static/(?P<path>.*)$', views.serve())
-    ]
+    url(r'^admin/', admin.site.urls)
+    ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
